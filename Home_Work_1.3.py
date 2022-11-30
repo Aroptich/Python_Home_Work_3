e41700dd2998248e4572f583f6acfb7829eddf73
Alexsandr <artamoshkinas@gmail.com>
2022-11-30 13:50:02 +0700

    Задание 3 выполнено!

--- Home_Work_1.3.py
+++ Home_Work_1.3.py
@@ -1,1 +1,9 @@
+# Задайте список из вещественных чисел.
+# Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+def dif_between_the_max_and_min_value(list_a) -> float:
+    if type(list_a) in [list] and all(type(x) for x in list_a):
+        temp = list(map(lambda x: round((float(x) - int(x)), 2), list_a))
+        return max(temp)-min(temp)
+    return -1
 
+dif_between_the_max_and_min_value([1.1, 1.2, 3.1, 10.01])
